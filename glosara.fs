89badeb4797cff8e0494290bbbb64b613c4db31c
programandala.net <github@programandala.net>
2016-04-28 22:36:12 +0000

    Homogenize the format comments
    FossilOrigin-Name: 9784e188a595e54e3d82284195cc893631a4446ae2e14d42022998f3a8bdd23a

--- glosara.fs
+++ glosara.fs
@@ -4,7 +4,7 @@
 
 \ XXX UNDER DEVELOPMENT
 
-: version  s" 0.1.0+201604290020" ;
+: version  s" 0.1.0+201604290035" ;
 
 \ ==============================================================
 \ Description
@@ -62,7 +62,7 @@ s" .glossary_entry" 2constant entry-filename-extension
 
 : echo  ( ca len -- )
   verbose @ if  type cr  else  2drop  then  ;
-  \ print the string _ca len_ if `verbose` is on
+  \ Print the string _ca len_ if `verbose` is on.
 
 \ ==============================================================
 \ Word lists
@@ -71,36 +71,35 @@ s" .glossary_entry" 2constant entry-filename-extension
   only forth  ;
 
 0 value extract-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
-  \ a source file
+  \ Words recognized during the interpretation of a source file.
 
 0 value entry-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
-  \ a glossary entry in a source file
+  \ Words recognized during the interpretation of
+  \ a glossary entry in a source file.
 
 : set-source-order  ( -- )
   extract-wordlist >order seal  ;
-  \ set the search order needed to parse the source
+  \ Set the search order needed to parse the source.
 
 : set-entry-order  ( -- )
   entry-wordlist >order seal  ;
-  \ set the search order needed to parse a glossary entry
+  \ Set the search order needed to parse a glossary entry
 
 wordlist constant forth-extract-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
+  \ Words recognized during the interpretation of
   \ a Forth source file
 
 wordlist constant z80-extract-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
-  \ a Z80 source file
+  \ Words recognized during the interpretation of
+  \ a Z80 source file.
 
 wordlist constant forth-entry-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
-  \ a glossary entry in a Forth source file
+  \ Words recognized during the interpretation of
+  \ a glossary entry in a Forth source file.
 
 wordlist constant z80-entry-wordlist  ( -- wid )
-  \ words recognized during the interpretation of
-  \ a glossary entry in a Z80 source file
+  \ Words recognized during the interpretation of
+  \ a glossary entry in a Z80 source file.
 
 \ ==============================================================
 \ Files
@@ -122,24 +121,25 @@ wordlist constant z80-entry-wordlist  ( -- wid )
 
 : c>hex  ( c -- ca len )
   base @ >r hex  s>d <# # # #>  r> base !  ;
-  \ convert a character to a 2-char string with its hex value
+  \ Convert a character to a 2-char string with its hex value.
 
 : string>hex  ( ca1 len1 -- ca2 len2 )
   s" " 2swap bounds do  i c@ c>hex s+  loop  ;
-  \ _ca2 len2_ consists of the hex values of the characters of
-  \ _ca1 len1_ (one 2-digit hex number per original character)
+  \ Return a string _ca2 len2_ which consists of the hex values
+  \ of the characters of string _ca1 len1_ (one 2-digit hex
+  \ number per original character).
 
 : entry-files-pattern  ( -- ca len )
   temp-directory s" *" s+ entry-filename-extension s+  ;
-  \ wildcard pattern for all temporary entry files
+  \ Wildcard pattern for all temporary entry files.
 
 : delete-temp-files  ( -- )
   entry-files-pattern  s" rm -f " 2swap s+ system  ;
-  \ delete all temporary entry files
+  \ Delete all temporary entry files.
 
 : ruler  ( c len -- ca len )
   dup allocate throw swap 2dup 2>r rot fill 2r>  ;
-  \ return a string of _len_ characters _c_.
+  \ Return a string of _len_ characters _c_.
 
 31 constant max-word-length
 
@@ -148,10 +148,10 @@ wordlist constant z80-entry-wordlist  ( -- wid )
   2swap string>hex s+ [ max-word-length 2 * ] literal string/
   entry-filename-extension s+
   temp-directory 2swap s+  ;
-  \ convert the name of a glossary entry _ca1 len1_ (a Forth
-  \ word) to its temporary filename _ca2 len2_. the base
-  \ filename consists of 31 8-bit hex numbers that represent the
-  \ characters of the entry name.
+  \ Convert the name of a glossary entry _ca1 len1_ (a Forth
+  \ word) to its temporary filename _ca2 len2_. The base
+  \ filename consists of `max-word-length` 8-bit hex numbers
+  \ that represent the characters of the entry name.
 
 \ ==============================================================
 \ Glossary entries
@@ -159,23 +159,23 @@ wordlist constant z80-entry-wordlist  ( -- wid )
 : end-of-glossary-entry?  ( ca len -- f )
   \ cr ." End of entry? " 2dup type key drop  \ XXX INFORMER
   s" }doc" str=  ;
-  \ is the given string the end of a glossary entry?
+  \ Is the given string the end of a glossary entry?
 
 : end-of-glossary-entry  ( -- )
   \ cr ." End of entry -- press any key" key drop  \ XXX INFORMER
   0 parse 2drop  \ discard the rest of the line
   close-entry-file set-source-order  ;
-  \ end of the current glossary entry
+  \ End of the current glossary entry.
 
 : new-entry  ( ca len -- )
   entryname>filename create-entry-file
   s" == " >entry-file  ;
-  \ start a new entry, creating its output file
+  \ Start a new entry, creating its output file
 
 : get-entry-line  ( "text<eol>" -- )
   \ ." valid-entry-line: " 2dup type ~~ cr key drop  \ XXX INFORMER
   0 parse >entry-file-line cr  ;
-  \ parse a line of the current entry and write it to its file
+  \ Parse a line of the current entry and write it to its file.
 
 : entry-line  ( "text<eol>" -- )
   save-input parse-name
@@ -185,26 +185,26 @@ wordlist constant z80-entry-wordlist  ( -- wid )
   dup 0<> entry-fid 0= and
   if  new-entry  else  2drop  then
   restore-input throw  get-entry-line  ;
-  \ parse and type a line of glossary entry
+  \ Parse and type a line of glossary entry.
 
 \ ==============================================================
 \ Interface
 
 forth-extract-wordlist set-current
 : doc{  ( -- )  set-entry-order  ;
-  \ start a glossary entry
+  \ Start a glossary entry.
 
 forth-entry-wordlist set-current
 : \  ( "text<eol>"  -- )  entry-line  ;
-  \ start of glossary entry line in a Forth source
+  \ Start of glossary entry line in a Forth source.
 
 z80-extract-wordlist set-current
 : doc{  ( -- )  set-entry-order  ;
-  \ start a glossary entry
+  \ Start a glossary entry.
 
 z80-entry-wordlist set-current
 : ;  ( "text<eol>"  -- )  entry-line  ;
-  \ start of glossary entry line in a Z80 source
+  \ Start of glossary entry line in a Z80 source.
 
 \ ==============================================================
 \ Parser
@@ -215,8 +215,8 @@ forth-wordlist set-current
   2dup s" .z80s" string-suffix? if  2drop true exit  then
        s" .asm" string-suffix? if  true exit  then
   false  ;
-  \ is the filename _ca len_ a Z80 source?
-  \ if not, it's supposed to be a Forth source
+  \ Is the filename _ca len_ a Z80 source?
+  \ If not, it's supposed to be a Forth source.
 
 : parse-current-source  ( -- )
   begin  refill  while
@@ -225,32 +225,32 @@ forth-wordlist set-current
       find-name ?dup if  name>int execute  then
     repeat  2drop
   repeat  ;
-  \ parse the current source file
+  \ Parse the current source file.
 
 : parse-file  ( fid -- )
   set-source-order
   ['] parse-current-source execute-parsing-file
   set-normal-order  ;
-  \ parse the file _fid_
+  \ Parse the file _fid_.
 
 : get-wordlists  ( ca len -- wid1 wid2 )
   z80-source-file?
   if    z80-extract-wordlist z80-entry-wordlist
   else  forth-extract-wordlist forth-entry-wordlist
   then  ;
-  \ return the proper word lists for the source file _ca len_
+  \ Return the proper word lists for the source file _ca len_.
 
 : set-wordlists  ( ca len -- )
   get-wordlists  to entry-wordlist  to extract-wordlist  ;
-  \ set the proper word lists for the source file _ca len_
+  \ Set the proper word lists for the source file _ca len_.
 
 : parse-source-file  ( ca len -- )
   2dup set-wordlists  r/o open-file throw
   dup parse-file
   close-file  \ XXX FIXME double free or corruption error
   throw  ;
-  \ extract the glossary information from file _ca len_ and
-  \ print it to standard output
+  \ Extract the glossary information from file _ca len_ and
+  \ print it to standard output.
 
 \ ==============================================================
 \ Argument parser
@@ -283,12 +283,12 @@ false  \ switch type
 arg.output-option arguments arg-add-option
 
 : help  ( -- )
-  \ Show the help
   arguments arg-print-help  ;
+  \ Show the help.
 
 : aid  ( -- )
   options @ ?exit  help  ;
-  \ show the help if no option was specified
+  \ Show the help if no option was specified.
 
 : verbose-option  ( -- )
   verbose on  s" Verbose mode is on" echo  ;
@@ -315,7 +315,7 @@ arg.output-option arguments arg-add-option
 
 : option?  ( -- n f )
   arguments arg-parse  dup arg.done <> over arg.error <> and  ;
-  \ parse the next option. is it right?
+  \ Parse the next option. Is it right?
 
 \ ==============================================================
 \ Boot
