218b6fd095530d524f5e1ab176cffb8140a49c03
programandala.net <github@programandala.net>
2016-04-28 21:38:50 +0000

    Update from development backup 201511260400
    FossilOrigin-Name: 0f5efcd174c2701278d740a78818ec19af1b913651070b7cdf85f17d49bfae22

--- glosser.fs
+++ glosser.fs
@@ -41,6 +41,10 @@ s" 20151125" 2constant version
 \ vocabularies for parsing.
 \
 \ 2015-11-25: First draft.
+\
+\ 2015-11-26: First working version: the glossaries are created
+\ from the sources of Solo Forth. Now the entries must be
+\ ordered.
 
 \ ==============================================================
 \ Requirements
@@ -57,6 +61,12 @@ require galope/string-suffix-question.fs
 \ ==============================================================
 \ Source interpreter
 
+: set-normal-order  ( -- )
+  only forth  ;
+
+2variable entry-name  \ XXX TODO -- not used yet
+  \ string of the glossary entry name
+
 0 value extract-wordlist  ( -- wid )
   \ words recognized during the interpretation of
   \ a source file
@@ -64,6 +74,14 @@ require galope/string-suffix-question.fs
 0 value entry-wordlist  ( -- wid )
   \ words recognized during the interpretation of
   \ a glossary entry in a source file
+
+: set-source-order  ( -- )
+  extract-wordlist >order seal  ;
+  \ set the search order needed to parse the source
+
+: set-entry-order  ( -- )
+  entry-wordlist >order seal  ;
+  \ set the search order needed to parse a glossary entry
 
 wordlist constant forth-extract-wordlist  ( -- wid )
   \ words recognized during the interpretation of
@@ -82,57 +100,83 @@ wordlist constant z80-entry-wordlist  ( -- wid )
   \ a glossary entry in a Z80 source file
 
 : end-of-glossary-entry?  ( ca len -- f )
-  cr ." End of entry? " 2dup type key drop  \ XXX INFORMER
+  \ cr ." End of entry? " 2dup type key drop  \ XXX INFORMER
   s" }doc" str=  ;
   \ is the given string the end of a glossary entry?
 
 : end-of-glossary-entry  ( -- )
-  cr ." End of entry -- press any key" key drop  \ XXX INFORMER
+  \ cr ." End of entry -- press any key" key drop  \ XXX INFORMER
   0 parse 2drop  \ discard the rest of the line
-  extract-wordlist >order seal  ;
+  set-source-order  
+  \ ~~ cr ." end of entry " order key drop \ XXX INFORMER
+  ;
 
-: glossary-line  ( "text<eol>" -- )
-  parse-name 2dup end-of-glossary-entry?
+  \ XXX TODO
+  \
+: c>hex  ( c -- ca len )
+  base @ >r hex  s>d <# # # #>  r> base !  ;
+  \ convert a character to a 2-char string with its hex value
+
+: string>hex  ( ca1 len1 -- ca2 len2 )
+  s" " 2swap bounds do  i c@ c>hex s+  loop  ;
+  \ _ca2 len2_ consists of the hex values of the characters of
+  \ _ca1 len1_ (one 2-digit hex number per original character)
+
+: empty-entry-name?  ( -- f )
+  entry-name 2@ or 0=  ;
+  \ is the name of the current entry empty?
+
+: set-entry-name  ( ca len -- )
+  \ XXX FIXME -- empty string!
+  2dup cr ." ENTRY=" type cr key drop \ XXX INFORMER
+  save-mem entry-name 2!  ;
+  \ set the name of the current entry
+
+: valid-entry-line  ( ca len -- )
+  empty-entry-name? if  2dup set-entry-name  then
+  type space 0 parse type cr  ;
+  \ print a valid line of the entry, whose first 
+  \ word _ca len_ has been already parsed
+
+: entry-line  ( "text<eol>" -- )
+  parse-name ~~ 2dup end-of-glossary-entry?
+  ~~
   if    2drop end-of-glossary-entry
-  else  type space 0 parse type
-  then  ;
-  \ parse and type a glossary line
+  else  ~~ valid-entry-line  then  ;
+  \ parse and type a line of glossary entry
 
-: (doc{)  ( -- )
-  entry-wordlist >order seal  ;
+: start-entry  ( -- )
+  0. entry-name 2!  \ empty the entry name
+  set-entry-order  ;
   \ start a glossary entry
 
 \ ----------------------------------------------
 
 forth-extract-wordlist set-current
 
-: doc{  ( -- )
-  \ cr ." Entry found -- press any key" key drop  \ XXX INFORMER
-  (doc{)  ;
+: doc{  ( -- )  start-entry  ;
   \ start a glossary entry
 
 \ ----------------------------------------------
 
 z80-extract-wordlist set-current
 
-: doc{  ( -- )
-  cr ." Entry found -- press any key" key drop  \ XXX INFORMER
-  (doc{)  ;
+: doc{  ( -- )  start-entry  ;
   \ start a glossary entry
 
 \ ----------------------------------------------
 
 forth-entry-wordlist set-current
 
-: \  ( "text<eol>"  -- )  glossary-line  ;
-  \ start of glossary line
+: \  ( "text<eol>"  -- )  entry-line  ;
+  \ start of glossary entry line in a Forth source
 
 \ ----------------------------------------------
 
 z80-entry-wordlist set-current
 
-: ;  ( "text<eol>"  -- )  glossary-line  ;
-  \ Start of line of glossary entry.
+: ;  ( "text<eol>"  -- )  entry-line  ;
+  \ start of glossary entry line in a Z80 source
 
 \ ==============================================================
 \ File converter
@@ -146,44 +190,36 @@ forth-wordlist set-current
   \ is the filename _ca len_ a Z80 source?
   \ if not, it's supposed to be a Forth source
 
-: (parse-source)  ( -- )
-  begin   parse-name
-          \ 2dup type space  \ XXX INFORMER
-          dup
-  while   find-name ?dup if  name>int execute  then
-  repeat  2drop  ;
-  \ parse the current source
+: (parse-file)  ( -- )
+  begin  refill  while
+    begin  parse-name dup  while
+      \ 2dup ." <" type ." > " \ key drop  \ XXX INFORMER
+      find-name ?dup if  name>int execute  then
+    repeat  2drop
+  repeat  ;
+  \ parse the current file
 
-: restore-order  ( -- )
-  only forth  ;
-
-: parse-source  ( ca len -- )
-  extract-wordlist >order seal
-  ['] (parse-source) execute-parsing
-  restore-order  ;
+: parse-file  ( fid -- )
+  set-source-order
+  ['] (parse-file) execute-parsing-file
+  set-normal-order  ;
+  \ parse the file _fid_
 
 : get-wordlists  ( ca len -- wid1 wid2 )
   z80-source-file?
   if    z80-extract-wordlist z80-entry-wordlist
   else  forth-extract-wordlist forth-entry-wordlist
   then  ;
-  \ return the proper wordlist for the source file _ca len_
+  \ return the proper word lists for the source file _ca len_
 
 : set-wordlists  ( ca len -- )
   get-wordlists  to entry-wordlist  to extract-wordlist  ;
-  \ set the proper wordlist for the source file _ca len_
+  \ set the proper word list for the source file _ca len_
 
 : parse-source-file  ( ca len -- )
-  2dup set-wordlists  slurp-file parse-source  ;
-  \ get the contents of the source file _ca len_, extract the
-  \ glossary information and print it to standard output
-
-: glosser  ( ca len -- )
-  \ ." About to parse " 2dup type cr  \ XXX INFORMER
-  \ ." Press any key" key drop  \ XXX INFORMER
-  2>r  get-order  2r> parse-source-file  set-order  ;
-  \ extract the glossary information from the source file
-  \ _ca len_ and print it to standard output
+  2dup set-wordlists  r/o open-file throw  parse-file  ;
+  \ extract the glossary information from file _ca len_ and
+  \ print it to standard output
 
 : about  ( -- )
   ." glosser" cr
@@ -203,9 +239,9 @@ forth-wordlist set-current
 
 : run  ( -- )
   input-files ?dup
-  if    0 do  i 1+ arg glosser  loop
-  else  about  then  ;
+  if    0 do  i 1+ arg parse-source-file  loop  argc off
+  else  about  then  set-normal-order  ;
 
-run
+run 
 
 \ vim: textwidth:64
