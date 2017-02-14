2556c83558181303cc6331720697f955f22b8454
programandala.net <github@programandala.net>
2017-02-14 14:21:50 +0000

    Rewrite the parser
    Processing the input as text lines will be simpler than parsing it as
    code, in this case.
    
    FossilOrigin-Name: d3b064ce3ac510b42e96f080d6ab91a73c9e4b6f318f057d44bb853ed3754748

--- glosara.fs
+++ glosara.fs
@@ -4,7 +4,7 @@
 
 \ XXX UNDER DEVELOPMENT
 
-: version  s" 0.1.0+201701262203" ;
+: version  s" 0.2.0+201702141520" ;
 
 \ ==============================================================
 \ Description
@@ -19,7 +19,7 @@
 \ ==============================================================
 \ Author and license
 
-\ Copyright (C) 2015, 2016 Marcos Cruz (programandala.net)
+\ Copyright (C) 2015, 2016, 2017 Marcos Cruz (programandala.net)
 
 \ You may do whatever you want with this work, so long as you
 \ retain the copyright notices and this license in all
@@ -38,7 +38,7 @@ forth-wordlist set-current
 \ From Forth Foundation Library
 \ (http://irdvo.github.io/ffl/)
 
-include ffl/arg.fs  \ argument parser
+include ffl/arg.fs \ argument parser
 
 \ From the Galope library
 \ (http://programandala.net/en.program.galope.html)
@@ -47,12 +47,14 @@ require galope/unslurp-file.fs
 require galope/minus-extension.fs
 require galope/string-suffix-question.fs
 require galope/string-slash.fs
+require galope/trim.fs
+require galope/slash-name.fs
 
 \ ==============================================================
 \ Config
 
-variable verbose      \ flag: verbose mode? \ XXX not used yet
-variable options      \ counter: valid options on the command line
+variable verbose verbose off \ flag: verbose mode?
+variable options \ counter: valid options on the command line
 
 s" /tmp/" 2constant temp-directory
 s" .glossary_entry" 2constant entry-filename-extension
@@ -60,94 +62,78 @@ s" .glossary_entry" 2constant entry-filename-extension
 \ ==============================================================
 \ Misc
 
-: echo  ( ca len -- )
-  verbose @ if  type cr  else  2drop  then  ;
+: echo ( ca len -- )
+  verbose @ if  type cr  else  2drop  then ;
   \ Print the string _ca len_ if `verbose` is on.
-
-\ ==============================================================
-\ Word lists
-
-: set-normal-order  ( -- )
-  only forth  ;
-
-0 value extract-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of a source file.
-
-0 value entry-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of
-  \ a glossary entry in a source file.
-
-: set-source-order  ( -- )
-  extract-wordlist >order seal  ;
-  \ Set the search order needed to parse the source.
-
-: set-entry-order  ( -- )
-  entry-wordlist >order seal  ;
-  \ Set the search order needed to parse a glossary entry
-
-wordlist constant forth-extract-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of
-  \ a Forth source file
-
-wordlist constant z80-extract-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of
-  \ a Z80 source file.
-
-wordlist constant forth-entry-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of
-  \ a glossary entry in a Forth source file.
-
-wordlist constant z80-entry-wordlist  ( -- wid )
-  \ Words recognized during the interpretation of
-  \ a glossary entry in a Z80 source file.
 
 \ ==============================================================
 \ Files
 
+2variable output-filename \ filename
+
+0 [if] \ XXX TODO --
+
 0 value entry-fid
-2variable output-filename  \ filename
 
-: create-entry-file  ( ca len -- )
-  w/o create-file throw  to entry-fid  ;
+: create-entry-file ( ca len -- )
+  w/o create-file throw  to entry-fid ;
 
-: close-entry-file  ( -- )
-  entry-fid close-file throw  0 to entry-fid  ;
+: close-entry-file ( -- )
+  entry-fid close-file throw  0 to entry-fid ;
 
-: >entry-file  ( ca len -- )
-  entry-fid write-file throw  ;
+: >entry-file ( ca len -- )
+  entry-fid write-file throw ;
 
-: >entry-file-line  ( ca len -- )
-  entry-fid ?dup if  write-line throw  else  2drop  then  ;
+: >entry-file-line ( ca len -- )
+  entry-fid ?dup if  write-line throw  else  2drop  then ;
 
-: c>hex  ( c -- ca len )
-  base @ >r hex  s>d <# # # #>  r> base !  ;
+[then]
+
+0 [if] \ XXX TODO --
+
+: create-output-file ( ca len -- )
+  w/o create-file throw  to output-fid ;
+
+: close-output-file ( -- )
+  output-fid close-file throw  0 to output-fid ;
+
+: >output-file ( ca len -- )
+  output-fid write-file throw ;
+
+: >output-file-line ( ca len -- )
+  output-fid ?dup if  write-line throw  else  2drop  then ;
+
+[then]
+
+: c>hex ( c -- ca len )
+  base @ >r hex  s>d <# # # #>  r> base ! ;
   \ Convert a character to a 2-char string with its hex value.
 
-: string>hex  ( ca1 len1 -- ca2 len2 )
-  s" " 2swap bounds do  i c@ c>hex s+  loop  ;
+: string>hex ( ca1 len1 -- ca2 len2 )
+  s" " 2swap bounds do  i c@ c>hex s+  loop ;
   \ Return a string _ca2 len2_ which consists of the hex values
   \ of the characters of string _ca1 len1_ (one 2-digit hex
   \ number per original character).
 
-: entry-files-pattern  ( -- ca len )
-  temp-directory s" *" s+ entry-filename-extension s+  ;
+: entry-files-pattern ( -- ca len )
+  temp-directory s" *" s+ entry-filename-extension s+ ;
   \ Wildcard pattern for all temporary entry files.
 
-: delete-temp-files  ( -- )
-  entry-files-pattern  s" rm -f " 2swap s+ system  ;
+: delete-temp-files ( -- )
+  entry-files-pattern  s" rm -f " 2swap s+ system ;
   \ Delete all temporary entry files.
 
-: ruler  ( c len -- ca len )
-  dup allocate throw swap 2dup 2>r rot fill 2r>  ;
+: ruler ( c len -- ca len )
+  dup allocate throw swap 2dup 2>r rot fill 2r> ;
   \ Return a string of _len_ characters _c_.
 
 31 constant max-word-length
 
-: entryname>filename  ( ca1 len1 -- ca2 len2 )
+: entryname>filename ( ca1 len1 -- ca2 len2 )
   '0' max-word-length ruler
   2swap string>hex s+ [ max-word-length 2 * ] literal string/
   entry-filename-extension s+
-  temp-directory 2swap s+  ;
+  temp-directory 2swap s+ ;
   \ Convert the name of a glossary entry _ca1 len1_ (a Forth
   \ word) to its temporary filename _ca2 len2_. The base
   \ filename consists of `max-word-length` 8-bit hex numbers
@@ -156,117 +142,76 @@ wordlist constant z80-entry-wordlist  ( -- wid )
 \ ==============================================================
 \ Glossary entries
 
-: end-of-glossary-entry?  ( ca len -- f )
-  \ cr ." End of entry? " 2dup type key drop  \ XXX INFORMER
-  s" }doc" str=  ;
-  \ Is the given string the end of a glossary entry?
+0 [if] \ XXX OLD
 
-: end-of-glossary-entry  ( -- )
-  \ cr ." End of entry -- press any key" key drop  \ XXX INFORMER
-  0 parse 2drop  \ discard the rest of the line
-  close-entry-file set-source-order  ;
+: parse-line ( -- ca len )  0 parse ;
+
+: end-of-entry ( -- )
+  ." }doc found! End of entry." \ XXX INFORMER
+  \ cr ." End of entry -- press any key" key drop \ XXX INFORMER
+  close-entry-file set-source-order ;
   \ End of the current glossary entry.
 
-: new-entry  ( ca len -- )
+: new-entry ( ca len -- )
   entryname>filename create-entry-file
-  s" == " >entry-file  ;
+  s" == " >entry-file ;
   \ Start a new entry, creating its output file.
 
-: get-entry-line  ( "text<eol>" -- )
-  \ ." valid-entry-line: " 2dup type ~~ cr key drop  \ XXX INFORMER
-  0 parse >entry-file-line cr  ;
+: get-entry-line ( "text<eol>" -- )
+  \ ." valid-entry-line: " 2dup type ~~ cr key drop \ XXX INFORMER
+  parse-line >entry-file-line cr ;
   \ Parse a line of the current entry and write it to its file.
 
-: parse-line  ( -- ca len )  0 parse  ;
-
-: entry-line  ( "text<eol>" -- )
-  \ XXX FIXME -- why parse names instead of the whole line?
-  \ XXX FIXME -- why `save-input`?
-  save-input parse-line
-\  ~~ ." parsed: " 2dup type cr  \ XXX INFORMER
-  \ key drop  \ XXX INFORMER
-  2dup end-of-glossary-entry?
-  if  2drop restore-input throw end-of-glossary-entry exit  then
-  dup 0<> entry-fid 0= and
-  if  new-entry  else  2drop  then
-  restore-input throw  get-entry-line  ;
+: parse-entry-line ( "text<eol>" -- )
+  parse-line
+  2dup ." «" type ." »" \ XXX INFORMER
+  \ key drop \ XXX INFORMER
+  2dup end-of-entry? if   end-of-entry
+                     else type then ;
   \ Parse and type a line of glossary entry.
 
-\ ==============================================================
-\ Interface
-
-forth-extract-wordlist set-current
-: doc{  ( -- )  set-entry-order  ;
-  \ Start a glossary entry.
-
-forth-entry-wordlist set-current
-: \  ( "text<eol>"  -- )  entry-line  ;
-  \ Start of glossary entry line in a Forth source.
-
-z80-extract-wordlist set-current
-: doc{  ( -- )
-\  ~~  \ XXX INFORMER
-  set-entry-order
-\  ~~  \ XXX INFORMER
-  ;
-  \ Start a glossary entry.
-
-z80-entry-wordlist set-current
-: ;  ( "text<eol>"  -- )
-  \ ~~  \ XXX INFORMER
-  entry-line
-  \ ~~  \ XXX INFORMER
-  ;
-  \ Start of glossary entry line in a Z80 source.
+[then]
 
 \ ==============================================================
-\ Parser
+\ Source parser
 
-forth-wordlist set-current
+255 constant /line-buffer
 
-: z80-source-file?  ( ca len -- f )
-  2dup s" .z80s" string-suffix? if  2drop true exit  then
-       s" .asm"  string-suffix?  ;
-  \ Is the filename _ca len_ a Z80 source?
-  \ If not, it's supposed to be a Forth source.
+create line-buffer /line-buffer 2 + chars allot
 
-: parse-current-source  ( -- )
-  begin  refill  while
-    begin  parse-name dup  while
-    \  ~~
-    2dup ." <" type ." > "  \ XXX INFORMER
-      \ key drop  \ XXX INFORMER
-      find-name ?dup if  name>int execute  then
-    repeat  2drop
-  repeat  ;
-  \ Parse the current source file.
+variable processing-entry \ flag: processing a glossary entry?
 
-: parse-file  ( fid -- )
-  set-source-order
-  cr ." parse-file 1" \ XXX INFORMER
-  ['] parse-current-source execute-parsing-file
-  cr ." parse-file 2" \ XXX INFORMER
-  set-normal-order  ;
-  \ Parse the file _fid_.
+: start-of-entry? ( ca len -- f )
+  s" doc{" str= ;
 
-: get-wordlists  ( ca len -- wid1 wid2 )
-  z80-source-file?
-  if    z80-extract-wordlist z80-entry-wordlist
-  else  forth-extract-wordlist forth-entry-wordlist
-  then  ;
-  \ Return the proper word lists for the source file _ca len_.
+: end-of-entry? ( ca len -- f )
+  s" }doc" str= ;
 
-: set-wordlists  ( ca len -- )
-  get-wordlists  to entry-wordlist  to extract-wordlist  ;
-  \ Set the proper word lists for the source file _ca len_.
+: process-ordinary-line ( ca len -- )
+  start-of-entry? processing-entry !  ;
 
-: parse-source-file  ( ca len -- )
-  \ cr ." parse-source-file " 2dup type \ XXX INFORMER
-  2dup set-wordlists  r/o open-file throw
-  \ cr ." parse-source-file open" \ XXX INFORMER
-  dup parse-file
-  close-file  \ XXX FIXME "double free or corruption" Gforth error
-  throw  ;
+: process-entry-line ( ca len -- )
+  2dup end-of-entry? if   processing-entry off 2drop
+                     else type cr then ;
+
+: tidy-line  ( ca len -- ca' len' )
+  /name 2nip trim ;
+ 
+: process-line ( ca len -- )
+  \ 2dup type cr \ XXX INFORMER
+  tidy-line processing-entry @ if   process-entry-line
+                               else process-ordinary-line then ;
+
+: read-line? ( fid -- ca len f )
+  >r line-buffer dup /line-buffer r> read-line throw ;
+
+: parse-file ( fid -- )
+  begin dup read-line? while process-line repeat 2drop ;
+  \ Extract the glossary information from file _fid_ and
+  \ print it to standard output.
+
+: parse-input-file ( ca len -- )
+  r/o open-file throw dup parse-file close-file throw ;
   \ Extract the glossary information from file _ca len_ and
   \ print it to standard output.
 
@@ -274,8 +219,8 @@ forth-wordlist set-current
 \ Argument parser
 
 \ Create a new argument parser
-s" Glosara"  \ name
-s" [ OPTION | INPUT-FILE ] ..."  \ usage
+s" Glosara" \ name
+s" [ OPTION | INPUT-FILE ] ..." \ usage
 version
 s" Written in Forth by Marcos Cruz (programandala.net)" \ extra
 arg-new constant arguments
@@ -286,44 +231,44 @@ arguments arg-add-version-option
 
 \ Add the verbose option
 4 constant arg.verbose-option
-char v  \ short option
-s" verbose"  \ long option
-s" activate verbose mode"  \ description
-true  \ switch type
+char v \ short option
+s" verbose" \ long option
+s" activate verbose mode" \ description
+true \ switch type
 arg.verbose-option arguments arg-add-option
 
 \ Add the output option
 5 constant arg.output-option
-char o  \ short option
-s" output"  \ long option
-s" set the output file"  \ description
-false  \ switch type
+char o \ short option
+s" output" \ long option
+s" set the output file" \ description
+false \ switch type
 arg.output-option arguments arg-add-option
 
-: help  ( -- )
-  arguments arg-print-help  ;
+: help ( -- )
+  arguments arg-print-help ;
   \ Show the help.
 
-: aid  ( -- )
-  options @ ?exit  help  ;
+: ?help ( -- )
+  options @ ?exit  help ;
   \ Show the help if no option was specified.
 
-: verbose-option  ( -- )
-  verbose on  s" Verbose mode is on" echo  ;
+: verbose-option ( -- )
+  verbose on  s" Verbose mode is on" echo ;
 
-: input-file  ( ca len -- )
+: input-file ( ca len -- )
   \ cr ." input-file " 2dup type \ XXX INFORMER
-  s" Processing " 2over s+ echo  parse-source-file  ;
+  s" Processing " 2over s+ echo  parse-input-file ;
 
-: output-option  ( ca len -- )
-  \ cr ." output-option " 2dup type  \ XXX INFORMER
+: output-option ( ca len -- )
+  \ cr ." output-option " 2dup type \ XXX INFORMER
   output-filename @ 0<> abort" More than one output file specified"
-  output-filename 2!  ;
+  output-filename 2! ;
 
-: version-option  ( -- )
-  arguments arg-print-version  ;
+: version-option ( -- )
+  arguments arg-print-version ;
 
-: option  ( n -- )
+: option ( n -- )
   1 options +!
   case
     arg.help-option       of  help              endof
@@ -331,21 +276,26 @@ arg.output-option arguments arg-add-option
     arg.output-option     of  output-option     endof
     arg.verbose-option    of  verbose-option    endof
     arg.non-option        of  input-file        endof
-  endcase  ;
+  endcase ;
 
-: option?  ( -- n f )
-  arguments arg-parse  dup arg.done <> over arg.error <> and  ;
+: option? ( -- n f )
+  arguments arg-parse  dup arg.done <> over arg.error <> and ;
   \ Parse the next option. Is it right?
 
 \ ==============================================================
 \ Boot
 
-: init  ( -- )
-  delete-temp-files
-  argc off  options off  verbose off  ;
+: init-variables ( -- )
+  processing-entry off ;
 
-: run  ( -- )
-  init  begin  option?  while  option  repeat  drop  aid  ;
+: init-arguments ( -- )
+  argc off  options off  verbose off ;
+
+: init ( -- )
+  delete-temp-files init-arguments init-variables ;
+
+: run ( -- )
+  init  begin  option?  while  option  repeat  drop  ?help ;
 
 run bye
 
@@ -363,7 +313,7 @@ run bye
 \ extracted from the sources of Solo Forth, ordered and joined
 \ into a glossary file. Added an argument parser.
 \
-\ 2016-04-28: Create Git repository out of the development
+\ 2016-04-28: Create a Git repository out of the development
 \ backups.  Rename to "Glosara". Change the version numbering
 \ after Semantic Versioning (http://semver.org).
 
